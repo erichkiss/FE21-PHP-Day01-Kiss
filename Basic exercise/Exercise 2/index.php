38fcd9aeb53c4f4fc48968c4f0cbb3a068ba9dd6
Erich Kiss <erich.kiss2@gmail.com>
2021-04-19 10:19:55 +0200

    Basic exercises done

---
+++ Basic exercise/Exercise 2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Day01 - Exercise 3</title>
+</head>
+
+
+<body>
+    <div>
+        <?php
+        $myname = "Erich Kiss";
+        $myage = "39";
+        $job_title = "Clown";
+
+        echo "Hi my name is $myname and I am $myage old and I work as a $job_title";
+        ?>
+    </div>
+</body>
+
+</html>
---
+++ Basic exercise/Exercise 3/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Day01 - Exercise 2</title>
+</head>
+
+
+<body>
+    <div>
+        <?php
+        $names = array("Mark", "John", "Georg", "Lisa");
+
+        echo "The third player in the team is $names[2]";
+        ?>
+    </div>
+</body>
+
+</html>
